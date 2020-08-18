281de114365e0675bcdbea815a48b1b011766dc0
Ayabonga <ayabongabooi2@gmail.com>
2020-08-18 14:13:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notes DDD With Firebase
+
+
+
+
---
+++ android/app/src/main/kotlin/com/ayabonga/notes_ddd_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ayabonga.notes_ddd_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
