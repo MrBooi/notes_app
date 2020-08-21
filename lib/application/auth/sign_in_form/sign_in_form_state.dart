287bb1b27d1dca19b6a7cedebda6291305121189
Ayabonga <ayabongabooi2@gmail.com>
2020-08-21 10:59:35 +0200

    added signIn form state and events

---
+++ lib/application/auth/sign_in_form/sign_in_form_state.dart
@@ -0,0 +1,20 @@
+part of 'sign_in_form_bloc.dart';
+
+@freezed
+abstract class SignInFormState with _$SignInFormState {
+  const factory SignInFormState({
+    @required EmailAdrdress emailAdrdress,
+    @required Password password,
+    @required bool showErrorMessages,
+    @required bool isSubmitting,
+    @required Option<Either<AuthFailure, Unit>> authFailureOrSuccessOption,
+  }) = _SignInFormState;
+
+  factory SignInFormState.initial() => SignInFormState(
+        emailAdrdress: EmailAdrdress(''),
+        password: Password(''),
+        showErrorMessages: false,
+        isSubmitting: false,
+        authFailureOrSuccessOption: none(),
+      );
+}
