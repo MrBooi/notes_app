ecb9a249c062e0ed105fc58d783ac018a8147b15
Ayabonga <ayabongabooi2@gmail.com>
2020-08-23 13:54:32 +0200

    add sign_in page

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Notes',
+        debugShowCheckedModeBanner: false,
+        builder: ExtendedNavigator(router: Router()),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.blue[900],
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_ddd_app/application/auth/sign_in_form/sign_in_form_bloc.dart';
+
+import '../../injection.dart';
+import 'widget/sign_in_form.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: BlocProvider(
+        create: (context) => getIt<SignInFormBloc>(),
+        child: SignInForm(),
+      ),
+    );
+  }
+}
